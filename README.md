407e71396f4fb86011664fce98ae061b966c81c5
MusannaGalib <musannagalib2016@gmail.com>
2024-02-15 15:09:58 -0800

    Bulding python package

---
+++ README.md
@@ -0,0 +1,29 @@
+# In-situ OM Image Tracking
+
+This repository contains scripts for tracking and analyzing in-situ optical microscopy (OM) images.
+
+## Overview
+
+The project aims to develop a package for tracking and analyzing bare zinc surfaces in in-situ OM images. It involves two main MATLAB scripts: `trracking_master_code.m` and `Tracked_surface_compare.m`.
+
+## Requirements
+
+- MATLAB R2022a or later
+- Python 3.x
+
+## Setup
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/MusannaGalib/Image_tracking.git
+
+
+# Navigate to the project directory
+cd Image_tracking
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+python run.py
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Image_tracking',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Musanna Galib, Matteo Ferraresso',
+    author_email='galibubc@student.ubc.ca, matfe@mail.ubc.ca',
+    description='Package for tracking and analyzing in-situ optical microscopy images',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MusannaGalib/Image_tracking.git',
+    license='MIT',
+    install_requires=[
+        # Add any Python dependencies required by your package
+    ],
+    extras_require={
+        'MATLAB': ['MATLAB>=R2022a']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
