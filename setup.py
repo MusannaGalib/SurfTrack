5d7f7bc64ff12e470d192c97c5843eb8e23c58bd
MusannaGalib <musannagalib2016@gmail.com>
2025-02-12 15:21:06 -0800

    pypi

--- setup.py
+++ setup.py
@@ -15,12 +15,9 @@ setup(
         # Add any Python dependencies required by your package
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3'
     ],
 )
